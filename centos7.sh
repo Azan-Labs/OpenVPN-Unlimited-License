fc808eda6bff57ad4dd2b706b7b759805d0465c0
Azan-Labs <azanulhaq212@gmail.com>
2024-10-31 16:36:58 +0500

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -8,11 +8,11 @@ echo "  \___\___|_||_\__|\___/|___//_/    "
 yum update -y
 yum install wget -y
 yum install net-tools -y
-wget https://repo.masiting.co/repo/openvpn-as-2.5-CentOS7.x86_64.rpm
+wget https://drive.google.com/file/d/1fl1JMAUVGZKiOi2hxfqbsBfMBpHkyT_u/view?usp=sharing
 rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
 cd /usr/local/openvpn_as/lib/python2.7/site-packages/
 rm pyovpn-2.0-py2.7.egg
-wget https://repo.masiting.co/repo/pyovpn-2.0-py2.7.egg
+wget https://drive.google.com/file/d/1SxSaRqYwEoetoD5k39TRNxPMYfuT21SQ/view?usp=drive_link
 cd /usr/local/openvpn_as/bin
 ./ovpn-init
 
